77a36c85e6fa01804a2befd37b4bb168a8385629
charis117 <manyac4@gmail.com>
2020-08-21 19:12:48 +0300

    a lot of changes

--- app/src/main/java/tools/chris/clip/MainActivity.java
+++ app/src/main/java/tools/chris/clip/MainActivity.java
@@ -15,81 +15,95 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity 
 {
-	
+
+	Socket s=null;
 	String text;
+	boolean searching=true;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		try{
 		text=getIntent().getExtras().getString(Intent.EXTRA_TEXT,"NOIN");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		if(text=="NOIN"){
 			ClipboardManager clip=(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
 			text=clip.getText().toString();
 		}
-		TextView tv=(TextView)findViewById(R.id.mainTextView);
 		
+		TextView tv=(TextView)findViewById(R.id.mainTextView);
+
 		tv.setText("Text to send:"+text);
 		
 		Thread th=new Thread(){
 			public void run(){
-				try {
-					Socket s=new Socket("192.168.1.45", 1456);
-					//OutputStream os=s.getOutputStream();
-					//os.write(text.length());
-					//os.write(text.getBytes());
-					
-				} catch (final IOException e) {
-					e.printStackTrace();
+			
+				while(s==null|searching){
+					for(int i=10;i<30;i++){
+						try{
+							s=new Socket("192.168.1."+i,1456);
+							searching=false;
+						}catch(IOException e){
+							e.printStackTrace();
+						}
+					}
+				}
+				if(s!=null){
 					runOnUiThread(new Runnable(){
 
 							@Override
-							public void run() {
-								Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+							public void run(){
+								EditText ed=(EditText)findViewById(R.id.mainEditText);
+								String ip=s.getInetAddress().getHostAddress();
+								
+								ed.setText(ip);
 							}
 
-
-						});
+						
+					});
+					
 				}
 
 
 			}
 		};
 		th.start();
-		
-		
+
+
     }
-	
-	
+
+
 	public void send(View v){
 		EditText ed=(EditText)findViewById(R.id.mainEditText);
 		final String IP=ed.getText().toString();
 		Thread th=new Thread(){
 			public void run(){
-				try {
-					Socket s=new Socket(IP, 1456);
+				try{
+					Socket s=new Socket(IP,1456);
 					OutputStream os=s.getOutputStream();
 					os.write(text.length());
 					os.write(text.getBytes());
 					runOnUiThread(new Runnable(){
 
 							@Override
-							public void run() {
+							public void run(){
 								sent();
 							}
 
-						
-					});
-				} catch (IOException e) {
+
+						});
+				}catch(IOException e){
 					e.printStackTrace();
 				}
-				
-				
+
+
 			}
 		};
 		th.start();
 	}
-	
+
 	public void sent(){
 		Toast.makeText(this,"Text sent",Toast.LENGTH_SHORT).show();
 	}
